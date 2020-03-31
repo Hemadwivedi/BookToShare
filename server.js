4f8c4de036e0868059d1020ea14a6302ac03afba
Ekaterina Blagoeva <erblagoeva@gmail.com>
2020-03-31 14:32:43 -0400

    added db, password code and changed listener

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 //requires
 var express = require("express");
 var exphbs = require("express-handlebars");
+var passport = require("./config/passport");
 
 //server port
 var app = express();
 var PORT = process.env.PORT || 8080;
+var db = require("./models");
 
 //the express app and everything that the app will use
 app.use(express.urlencoded({ extended: true }));
@@ -12,19 +14,18 @@ app.use(express.json());
 app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+// for loging
+app.use(session({ secret: "hemashirleyeti", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
-
-
-
-
-
-
-
 //listening port
-app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
+});
