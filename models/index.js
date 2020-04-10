35de721b37c73534e92853757a1bcb28fcdbf023
HDwivedi <hemaasheesh@gmail.com>
2020-04-09 21:22:02 -0400

    update  to run  based on env dev and prod

--- models/index.js
+++ models/index.js
@@ -10,25 +10,25 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable] , config);
+    var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config)
+    var sequelize = new Sequelize(config.database, config.username, config.password, config)
 }
 
 fs.readdirSync(__dirname)
     .filter(function (file) {
-      return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
     })
     .forEach(function (file) {
-      var model = sequelize['import'](path.join(__dirname, file));
-      console.log("Hello --- "  + model.name);
-      db[model.name] = model;
+        var model = sequelize['import'](path.join(__dirname, file));
+        console.log("Hello --- " + model.name);
+        db[model.name] = model;
     });
 
 Object.keys(db).forEach(function (modelName) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
 db.sequelize = sequelize;
